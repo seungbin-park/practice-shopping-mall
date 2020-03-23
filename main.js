d2e20c74db486f8eb9ec087f7799952f04837716
seungbin-park <tmdqls0212@naver.com>
2020-03-23 12:10:13 +0900

    homework

---
+++ main.js
@@ -0,0 +1,8 @@
+function (){
+    let name=$(#'order_name').val();
+    let name=$(#'order_count').val();
+    let name=$(#'order_address').val();
+    let name=$(#'order_phone').val();
+    if (name==''){alert('이름을 입력하세요')$('#order_name').return}
+
+}
